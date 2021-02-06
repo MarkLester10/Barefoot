a3c711640cbe190903c210446ad719166245fd34
Mark Lester Morta <marklester10.mlsm@gmail.com>
2021-02-06 19:13:40 +0800

    front -ne

--- forgot-password.php
+++ forgot-password.php
@@ -7,19 +7,8 @@ require_once ROOT_PATH . "/app/middlewares/GuestsMiddleware.php";
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Barefoot | Forgot Password</title>
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/imgs/favicon/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/assets/imgs/favicon/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  <link rel="manifest" href="/assets/imgs/favicon/site.webmanifest" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet" />
-  <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/scss/vendors/tailwind.css' ?>" />
-  <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
+  <?php include ROOT_PATH . '/app/includes/head.php' ?>
 </head>
 
 <body>
@@ -61,7 +50,7 @@ require_once ROOT_PATH . "/app/middlewares/GuestsMiddleware.php";
                   ?>
               </p>
               <?php else : ?>
-              <p class="py-2 text-gray-400">
+              <p class="py-2 text-sm text-gray-400">
                 Please enter the email address you've registered in our website, so we can assist you in recovering your
                 password.
               </p>
--- index.php
+++ index.php
@@ -6,45 +6,46 @@ require_once ROOT_PATH . "/app/Controllers/AuthController.php";
 <!doctype html>
 <html lang="en">
 
-<head>
 
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Barefoot | Reset Password</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/assets/imgs/favicon/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/imgs/favicon/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="manifest" href="/assets/imgs/favicon/site.webmanifest" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-      rel="stylesheet" />
-    <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/scss/vendors/tailwind.css' ?>" />
-    <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
-  </head>
+<head>
+  <title>Barefoot Travel Blog</title>
+  <?php include ROOT_PATH . '/app/includes/head.php' ?>
+</head>
 
 <body>
-  <div class="container mx-auto mt-24 w-1/2">
-    <div class="px-6 py-6 bg-gray-100">
-      <?php include ROOT_PATH . "/app/includes/messages.php"; ?>
-      <?php if (isset($_SESSION['id'])) : ?>
-      <h3>Welcome, <?php echo $_SESSION['username'] ?> </h3>
-      <a href="index.php?logout=1" class="text-red-400">Logout</a>
-      <?php if ($_SESSION['verified']) : ?>
-      <button class="px-2 py-3 rounded text-white mt-4 bg-green-400 block mx-auto">Your account has been verified! 🙂
-      </button>
-      <?php else : ?>
-      <div class="text-gray-900 bg-yellow-300 px-4 py-6">
-        You need to verify your account. 😞 <br><br>
-        Sign in to your email account and click on the verification link that we've just emailed to you at
-        <strong> <?php echo $_SESSION['email'] ?> </strong>
+  <header>
+    <nav class="navbar">
+      <div class="navbar__icon block lg:hidden mr-2">
+        <i class='bx bx-menu'></i>
       </div>
-      <?php endif; ?>
-      <?php else : ?>
-      <p>Travel bloggers are story tellers</p>
-      <?php endif; ?>
-    </div>
+      <div class="navbar__logo">
+        <a href="#"> <img src="./assets/imgs/logo__dark.png" alt="Barefoot"></a>
+      </div>
+      <div class="navbar__search">
+        <input
+          class="hidden lg:block border border-transparent bg-gray-100 text-gray-900 rounded-lg pl-10 pr-4 py-4 focus:outline-none focus:bg-white focus:border-gray-100 focus:text-gray-900 w-full dark:bg-gray-800 dark:focus:bg-gray-700 dark:focus:text-gray-100 dark:text-gray-100"
+          type="text" placeholder="Search by keywords" />
+      </div>
+      <div class="navbar__links">
+        <ul class="space-x-8">
+          <!-- <li><a href="#">Login</a></li>
+          <li><a href="#">Register</a></li> -->
+          <li class="profile space-x-6">
+            <img src="./assets/imgs/auth/avatar.png" class="w-20 hidden md:block" alt="">
+            <a href="#" class="flex items-center">Mark Lester<i class='bx bxs-chevron-down'></i></a>
+            <div class="profile__dropdown">
+              <ul>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Logout</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <div class="grid__container">
   </div>
 </body>
 
